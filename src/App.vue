3982fdbca1fe8b138175c94bbb4bbad939ee7b50
Volkov Alexander <volkov.525@gmail.com>
2020-11-26 13:16:53 +0300

    remove ts from app

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@
 </ion-app>
 </template>
 
-<script lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
-import { defineComponent } from 'vue';
+<script>
+import { IonApp, IonRouterOutlet } from '@ionic/vue'
+import { defineComponent } from 'vue'
 
 export default defineComponent({
 	name: 'App',
